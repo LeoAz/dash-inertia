7e3f42a7d83f7795bd6bb194dd07f99ef197db4b
Leo Az <lionel.aziangbe@gmail.com>
2025-11-20 19:44:46 +0000

    wip

--- resources/js/pages/settings/appearance.tsx
+++ resources/js/pages/settings/appearance.tsx
@@ -1,24 +1,21 @@
 import { Head } from '@inertiajs/react';
 
-import AppearanceTabs from '@/components/appearance-tabs';
 import HeadingSmall from '@/components/heading-small';
-import { type BreadcrumbItem } from '@/types';
 
 import AppLayout from '@/layouts/app-layout';
 import SettingsLayout from '@/layouts/settings/layout';
 
 export default function Appearance() {
     return (
-        <AppLayout >
+        <AppLayout>
             <Head title="Appearance settings" />
 
             <SettingsLayout>
                 <div className="space-y-6">
                     <HeadingSmall
                         title="Appearance settings"
-                        description="Update your account's appearance settings"
+                        description="Le mode sombre est désactivé et les paramètres d'apparence ne sont plus disponibles."
                     />
-                    <AppearanceTabs />
                 </div>
             </SettingsLayout>
         </AppLayout>
